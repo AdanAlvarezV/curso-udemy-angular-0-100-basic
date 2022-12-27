f6367968b9cc4a729d7fa6b31aa0c92ac1ce8cfb
AdanAlvarezV <ing.adanalvarez.valenzuela@gmail.com>
2022-12-27 17:38:29 -0600

    Primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { contadorComponent } from "./contador/contador.component";
+
+@NgModule({
+    declarations:[
+        contadorComponent
+    ], exports:[
+        contadorComponent
+    ]
+})
+export class ContadorModule{}
